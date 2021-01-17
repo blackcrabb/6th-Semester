7c1bb82e05578ec5e67b09e36c47a30f12282cf1
blackcrabb <57863214+blackcrabb@users.noreply.github.com>
2021-01-17 16:25:14 +0530

    Add files via upload

---
+++ Data Science Lab/sumton.R
@@ -0,0 +1,9 @@
+#sum of numbers taking from user
+n<-as.integer(readline("Enter no till whenever you wan to add : "))
+num<-0
+for(i in 1:n)
+{
+  num=num + i
+}
+print(num)
+
---
+++ Data Science Lab/table.R
@@ -0,0 +1,12 @@
+n<-as.integer(readline("Enter no for the table : "))
+tab<-1
+for(i in 1:12)
+{
+  tab = i * n
+  print(paste(n,"*",i,"=",tab))
+}
+
+
+
+
+
